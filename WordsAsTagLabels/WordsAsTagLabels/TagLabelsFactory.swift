532f79d294f0f98700f8f0c18edb73def8fe3f1d
Włodzimierz Woźniak <support@vivatum.com>
2020-06-04 21:37:34 +0200

    init commit

---
+++ WordsAsTagLabels/WordsAsTagLabels/TagLabelsFactory.swift
@@ -0,0 +1,20 @@
+//
+//  TagLabelsFactory.swift
+//  WordsAsTagLabels
+//
+//  Created by Vivatum on 04/06/2020.
+//  Copyright © 2020 com.vivatum. All rights reserved.
+//
+
+import UIKit
+
+final class TagLabelsFactory {
+    
+    let wordsArray: [String]
+    
+    init(wordsArray: [String]) {
+        self.wordsArray = wordsArray
+    }
+    
+    
+}
---
+++ WordsAsTagLabels/WordsAsTagLabels/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordsAsTagLabels
+//
+//  Created by Vivatum on 04/06/2020.
+//  Copyright © 2020 com.vivatum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
